1dada04f56d1706f26cde3293a5db4612f6913b4
chaudharykapil <chaudharykapil8279@gmail.com>
2021-06-21 11:29:41 +0530

    fix minnor errors

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from datetime import datetime
 with open('config.json', 'r') as c:
     params = json.load(c)["params"]
 
-local_server = True
+
 app = Flask(__name__)
 app.config.update(
     MAIL_SERVER = 'smtp.gmail.com',
@@ -16,7 +16,7 @@ app.config.update(
     MAIL_PASSWORD=  params['gmail-password']
 )
 mail = Mail(app)
-if(local_server):
+if([params["localserver"]]):
     app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
     #app.config['SQLALCHEMY_DATABASE_URI'] = params['kapil_database']
 else:
@@ -41,11 +41,11 @@ class Languages(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     name= db.Column(db.String(80), nullable=False)
     decription = db.Column(db.String(300), nullable=False)
-    website_link = db.Column(db.String(120), nullable=False)
-    download_link = db.Column(db.String(120), nullable=False)
-    documentation_link = db.Column(db.String(120), nullable=False)
+    website_link = db.Column(db.String(120), nullable=True)
+    download_link = db.Column(db.String(120), nullable=True)
+    documentation_link = db.Column(db.String(120), nullable=True)
     other_link = db.Column(db.String(120), nullable=True)
-    logo = db.Column(db.String(120), nullable=False)
+    logo = db.Column(db.String(120), nullable=True)
     slug = db.Column(db.String(100), nullable=False)
 
 
@@ -55,6 +55,8 @@ def home():
 @app.route("/post/<string:languages_slug>", methods=['GET'])
 def post(languages_slug):
     languages = Languages.query.filter_by(slug=languages_slug).first()
+    if not languages:
+        abort(404)
     return render_template('post.html',language=languages,params=params)
 
 @app.route("/about")
@@ -71,15 +73,15 @@ def login():
         password = request.form.get("password")
         #check if any user exist in database having same user id 
         user = Admin.query.filter_by(user_id = user_id).first()
-        if (user.user_id==user_id) or (user.password == password):
-                return render_template("add.html", params=params)
+        if (user.user_id==user_id) and (user.password == password):
+                return render_template("addLanguage.html", params=params)
         else:
             msg = "Email or Password may be wrong"
             return render_template("login.html", params=params,message = msg)
 
 @app.route("/contact", methods = ['GET', 'POST'])
 def contact():
-    if(request.method=='POST'):
+    if request.method == 'POST':
         name = request.form.get('name')
         email = request.form.get('email')
         phone = request.form.get('phone')
@@ -98,35 +100,39 @@ def contact():
 @app.route("/addlanguage",methods = ["GET","POST"])
 def AddLanguage():
     if request.method == "GET":
-        return render_template("add.html", params=params)
+        return render_template("addLanguage.html", params=params)
 
     if request.method == "POST":
         name = request.form.get("name")
+        #remove white spaces from end and begining of the name
+        name = name.strip()
         description = request.form.get("description")
         web_link = request.form.get("website_link")
         down_link = request.form.get("download_link")
         document_link = request.form.get("documentation_link")
         logo_link = request.form.get("logo_link")
-        language = Languages(
-            name = name,
-            decription = description,
-            website_link =  web_link,
-            download_link = down_link,
-            documentation_link = document_link,
-            logo = logo_link,
-            slug = str(name).replace(" ","_")
-        )
-        db.session.add(language)
-        db.session.commit()
-        msg = str(name)+" added Successfully"
-        return render_template("add.html", params=params,message = msg)
+        if Languages.query.filter_by(name = name).first():
+            msg = name+" already added"
+        else:
+            language = Languages(
+                name = name,
+                decription = description,
+                website_link =  web_link,
+                download_link = down_link,
+                documentation_link = document_link,
+                logo = logo_link,
+                slug = str(name).replace(" ","_")
+            )
+            db.session.add(language)
+            db.session.commit()
+            msg = str(name)+" added Successfully"
+        return render_template("addLanguage.html", params=params,message = msg)
 
 
 @app.route("/language/<string:lan>")
 def showLanguages(lan):
-    if lan == "":
-        abort(404)
-    elif lan == "all":
+    lan = lan.strip()
+    if lan == "all" or lan == "":
         languages = Languages.query.all()
     else:
         languages = Languages.query.filter_by(name = lan).all()
@@ -134,12 +140,19 @@ def showLanguages(lan):
         if not languages:
             return abort(404)
     return render_template("showlanguages.html",languages = languages)
+
+#right now not in use
+#for future development
 @app.route("/searchapi")
 def searchapi():
+    return abort(404)
     languages= Languages.query.all()
     language_name = list(map(lambda x: x.name,languages))
     language_dict = {}
     for x in range(len(language_name)):
         language_dict[x] = language_name[x]
     return language_dict
+
+
+
 app.run(debug=True)
